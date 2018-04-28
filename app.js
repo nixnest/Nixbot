05dcca188849b54a776ccf45c91960bbac981e58
Zwater <zack@zwater.us>
2018-04-28 17:00:32 -0400

    app.js
    	Added logging to a specific channel

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ client.on("message", async message => {
             });
         }}
     if (command ==="help") {
-        message.channel.send("**Help**\n\n**+tldr <channel>:** Returns emotions based on certain keywords in the last 100 messages in the channel (or the channel you specify. Optional)\n**+lmgtfy <query>:** Returns a link to lmgtfy for being passive aggressive.\n**+echo <words>:** duh.\n**+ping:** Returns diagnostic latency data, makes sure the bot's running.\n**+wiki <query>:** Searches the ArchWiki.");
+        message.channel.send("**Help**\n\n**+tldr <channel>:** Returns emotions based on certain keywords in the last 100 messages in the channel (or the channel you specify. Optional)\n**+lmgtfy <query>:** Returns a link to lmgtfy for being passive aggressive.\n**+echo <words>:** duh.\n**+ping:** Returns diagnostic latency data, makes sure the bot's running.\n**+wiki <query>:** Searches the ArchWiki.\n**+ban:** returns a nice message");
     }
     if (command === "ping") {
         // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
@@ -133,7 +133,14 @@ client.on("message", async message => {
         message.channel.send("https://wiki.archlinux.org/index.php?search=" + wiki);
     }
     if (command === "ban") {
-        message.channel.send("__**USER WAS BANNED FOR THIS POST**__");
+	message.channel.send("__**USER WAS BANNED FOR THIS POST**__");
+    }
+    if (command === "interject") {
+	message.channel.send("I'd just like to interject for a moment. What you\'re referring to as Linux, is in fact, GNU/Linux, or as I\'ve recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.\n\nMany computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called \"Linux\", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project. There really is a Linux, and these people are using it, but it is just a part of the system they use.\n\nLinux is the kernel: the program in the system that allocates the machine\'s resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called \"Linux\" distributions are really distributions of GNU/Linux. ");
+    };
+    message.delete();
+    if (message.guild.id.toString().includes('297744523910578176')) {
+    client.channels.get('439881143798595604').send(message.author + ' ran command `' + message.cleanContent + '` in ' + message.channel);
     }
 });
 
