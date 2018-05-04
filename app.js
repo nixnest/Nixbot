5329a2df5449209642d6d91a485cdd9f0eac6c21
Zwater <zachary.waser@gmail.com>
2018-05-03 21:55:20 -0400

    Added check for actual number to +message

--- app.js
+++ app.js
@@ -151,8 +151,10 @@ client.on("message", async message => {
                 arg.join();
                 var newmsg = arg.toString();
                 //message.channel.send("Setting your messages to " + newmsg + ". **You cannot do this again**");
-                console.log(message.member.roles.array());
-                if(message.member.roles.find('id', '297744523910578176') && newmsg < config.msgs_500[1]) {
+                //console.log(message.member.roles.array());
+                if (!isNaN(newmsg)) {
+                    message.channel.send("Provided message count `" + newmsg + "` does not appear to be a number. Try again.");
+                } else if (message.member.roles.find('id', '297744523910578176') && newmsg < config.msgs_500[1]) {
                     influx.writePoints([
                     {
                         measurement: 'message',
