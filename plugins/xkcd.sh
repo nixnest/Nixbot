995434a7f457b6b0665c6511454db3b952718439
kingofinfo <kingofinfo@users.noreply.github.com>
2018-05-07 16:53:11 -0700

    can now get xkcd comics with just +xkcd <comic number>, num is no longer needed

--- plugins/xkcd.sh
+++ plugins/xkcd.sh
@@ -6,11 +6,11 @@ case $1 in
     "rand")
         output=$(curl -s "https://xkcd.com/$rand/info.0.json")
         ;;
-    "num")
-        output=$(curl -s "https://xkcd.com/$2/info.0.json")
+    ''|*[!0-9]*)
+        output=$current
         ;;
     *)
-        output=$current
+        output=$(curl -s "https://xkcd.com/$1/info.0.json")
         ;;
 esac
 echo -e "#$(echo $output | jq -r '.num') $(echo $output | jq -r '.safe_title')\n$(echo $output | jq -r '.img')\n\n_$(echo $output | jq -r '.alt')_"
