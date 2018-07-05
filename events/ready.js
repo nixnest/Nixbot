9d5615fda79b8448e5c64fa7efae5d96d7a36c1e
Julian Webb <git@julianwebb.ca>
2018-07-05 05:16:25 -0700

    I guess I forgot to push ready.js

---
+++ events/ready.js
@@ -0,0 +1,6 @@
+// ready.js
+
+module.exports = async (config, client, influx) => {
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`)
+    client.user.setActivity('type +help')
+}
